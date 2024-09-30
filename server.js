f5660cd87e027fd7946fdbfba386ee07cb08d5cd
Mohammed <mrcloud.med@gmail.com>
2024-09-30 13:25:07 +0000

    Task

--- server.js
+++ server.js
@@ -1,14 +1,12 @@
-const express = require('express');
-const app = express();
+import express from 'express';
+import startServer from './libs/boot';
+import injectRoutes from './routes';
+import injectMiddlewares from './libs/middlewares';
 
-// Ensure you're requiring the correct module
-const routes = require('./routes/index'); 
+const server = express();
 
-const port = process.env.PORT || 5000;
+injectMiddlewares(server);
+injectRoutes(server);
+startServer(server);
 
-// Use the imported routes as middleware
-app.use('/', routes);
-
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-});
+export default server;
